b4e4984c59fce37fcb1a30b752e37b54e1650aa6
liloow <tom.beguin@gmail.com>
2017-11-24 11:29:09 +0100

    fixed invalid ingredient handling

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,39 +1,66 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const lab = require('../index')
+const lab = require("../index");
 
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
 	const labData = {
-		inventory: lab.inventory,
-	}
-  res.json(labData);
+		inventory: lab.inventory
+	};
+	res.json(labData);
 });
 
-router.post('/mix', function(req,res,next) {
+router.post("/mix", function(req, res, next) {
+	// if (Object.keys(req.body).length === 0)
+	// 	return res.json("Un ingredient doit toujours etre defini");
 
-	if (Object.keys(req.body).length===0) 	return res.json("Un ingredient doit toujours etre defini")
+	if (
+		req.body.ingredientOne &&
+		req.body.ingredientTwo &&
+		req.body.ingredientThree
+	) {
+		let { ingredientOne, ingredientTwo, ingredientThree } = req.body;
 
-	if (req.body.ingredientOne&&req.body.ingredientTwo&&req.body.ingredientThree) {
+		if (
+			typeof ingredientOne !== "string" ||
+			typeof ingredientTwo !== "string" ||
+			typeof ingredientThree !== "string"
+		)
+			return res.json("Les ingredients doivent etre des strings");
 
-	let {ingredientOne, ingredientTwo, ingredientThree} = req.body
+		if (
+			ingredientOne === ingredientTwo ||
+			ingredientOne === ingredientThree ||
+			ingredientTwo === ingredientThree
+		)
+			return res.json(
+				"Deux fois le meme ingredient a ete selectionne, la mixture est invalide"
+			);
 
-	if(typeof ingredientOne !=='string' || typeof ingredientTwo !=='string' || typeof ingredientThree!=='string') return res.json('Les ingredients doivent etre des strings')
+		fullIngredientOne =
+			lab.inventory[lab.inventory.map(el => el.name).indexOf(ingredientOne)];
+		fullIngredientTwo =
+			lab.inventory[lab.inventory.map(el => el.name).indexOf(ingredientTwo)];
+		fullIngredientThree =
+			lab.inventory[
+				lab.inventory.map(el => el.name).indexOf(ingredientThree)
+			];
 
-	if (ingredientOne === ingredientTwo || ingredientOne === ingredientThree || ingredientTwo === ingredientThree) return res.json('Deux fois le meme ingredient a ete selectionne, la mixture est invalide')
+			if (
+		!fullIngredientOne ||
+		!fullIngredientTwo ||
+		!fullIngredientThree
+	) return res.json("Un ou plusieurs ingredients envoye n'existe pas !")
 
+		const mixtureResult = lab.mix([
+			fullIngredientOne,
+			fullIngredientTwo,
+			fullIngredientThree
+		]);
 
-
-	ingredientOne=lab.inventory[lab.inventory.map(el=>el.name).indexOf(ingredientOne)]
-	ingredientTwo=lab.inventory[lab.inventory.map(el=>el.name).indexOf(ingredientTwo)]
-	ingredientThree=lab.inventory[lab.inventory.map(el=>el.name).indexOf(ingredientThree)]
-
-	const mixtureResult = lab.mix([ingredientOne, ingredientTwo, ingredientThree])
-
-	res.json(mixtureResult)
-}
-	else {
-		return res.json("Un ingredient doit toujours etre defini")
+		res.json(mixtureResult);
+	} else {
+		return next("Un ingredient doit toujours etre defini");
 	}
-})
+});
 
 module.exports = router;

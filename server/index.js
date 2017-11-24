5e4293dc0a104b32b6b4ac89ef738c19bbb342a0
liloow <tom.beguin@gmail.com>
2017-11-24 13:39:04 +0100

    deploy

--- server/index.js
+++ server/index.js
@@ -5,47 +5,92 @@ const Lab = require("./lab.js");
 let lab = new Lab();
 
 const validIngredients = [
-	new Ingredient("Vinaigre", 2),
+	new Ingredient("Vinaigre", 8),
 	new Ingredient("Persil", 5),
 	new Ingredient("Tete de Rat", 3),
 	new Ingredient("Bave de Lama", 8),
-	new Ingredient("Epines de herisson", 1),
+	new Ingredient("Epines de herisson", 14),
 	new Ingredient("Argent", 10),
-	new Ingredient("Or", 10),
-	new Ingredient("Diamant", 10),
+	new Ingredient("Or", 5),
+	new Ingredient("Diamant", 3),
 	new Ingredient("Fletan", 8)
 ];
 
 const validRecipes = [
-	new Recipe("Potion 1", [
+	new Recipe("Potion d\'invincibilité", [
 		validIngredients[1],
 		validIngredients[2],
 		validIngredients[3]
 	]),
-	new Recipe("Potion 2", [
+	new Recipe("Potion de force surhumaine", [
+		validIngredients[4],
+		validIngredients[5],
+		validIngredients[6]
+	]),
+	new Recipe("Potion d\'intelligence accrue", [
+		validIngredients[0],
 		validIngredients[2],
-		validIngredients[4],
-		validIngredients[3]
+		validIngredients[4]
 	]),
-	new Recipe("Potion 3", [
+	new Recipe("Potion d\'invisibilité", [
+		validIngredients[0],
 		validIngredients[3],
-		validIngredients[5],
-		validIngredients[7]
+		validIngredients[6]
 	]),
-	new Recipe("Potion 4", [
+	new Recipe("Elixir de vigueur", [
+		validIngredients[4],
+		validIngredients[2],
+		validIngredients[1]
+	]),
+	new Recipe("Elixir d\'amour éternel", [
+		validIngredients[6],
+		validIngredients[7],
+		validIngredients[8]
+	]),
+	new Recipe("Potion coupe-faim", [
 		validIngredients[4],
 		validIngredients[6],
-		validIngredients[4]
+		validIngredients[8]
 	]),
-	new Recipe("Potion 5", [
-		validIngredients[5],
+	new Recipe("Potion de chance", [
+		validIngredients[3],
 		validIngredients[7],
-		validIngredients[2]
+		validIngredients[8]
 	]),
-	new Recipe("Potion 6", [
+	new Recipe("Potion de soins", [
+		validIngredients[0],
+		validIngredients[1],
+		validIngredients[5]
+	]),
+	new Recipe("Potion de vitesse", [
+		validIngredients[1],
+		validIngredients[3],
+		validIngredients[5]
+	]),
+	new Recipe("Elixir de vie", [
+		validIngredients[0],
+		validIngredients[1],
+		validIngredients[8]
+	]),
+	new Recipe("Potion de mort instantanée", [
+		validIngredients[1],
 		validIngredients[6],
-		validIngredients[8],
-		validIngredients[4]
+		validIngredients[8]
+	]),
+	new Recipe("Elixir de jouvence", [
+		validIngredients[2],
+		validIngredients[5],
+		validIngredients[6]
+	]),
+	new Recipe("Polyjuice", [
+		validIngredients[4],
+		validIngredients[7],
+		validIngredients[8]
+	]),
+	new Recipe("Potion Magique", [
+		validIngredients[3],
+		validIngredients[6],
+		validIngredients[7]
 	])
 ];
 
@@ -61,4 +106,4 @@ console.log(lab.mix([lab.inventory[3],lab.inventory[1],lab.inventory[2]]))
 console.log(lab.mix([lab.inventory[0],lab.inventory[7],lab.inventory[8]]))
 console.log(lab.mix([lab.inventory[6],lab.inventory[8],lab.inventory[4]]))*/
 
-module.exports = lab
+module.exports = lab;
